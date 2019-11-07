883856b3ad1a45f18a84352462d27e47aabccb2f
Daniel Milnes <daniel.milnes@mwrinfosecurity.com>
2019-11-07 15:08:41 +0000

    Add bruteforce

--- hstsparser.py
+++ hstsparser.py
@@ -4,7 +4,23 @@ from prettytable import PrettyTable
 import os
 import re
 import json
-from time import sleep
+from hashlib import sha256
+from base64 import b64encode
+
+
+def convert(domain):
+    output = [chr(0)]
+    idx = 0
+    for char in reversed(domain):
+        if char == ".":
+            # output.append(my_hex(idx))
+            output.append(chr(idx))
+            idx = 0
+        else:
+            output.append(char)
+            idx += 1
+    output.append(chr(idx))
+    return b64encode(sha256("".join(reversed(output)).encode("utf-8")).digest())
 
 
 def printDB(database, field_names):
@@ -34,6 +50,13 @@ parser.add_argument(
     metavar="FILE",
     type=lambda x: is_valid_file(parser, x),
 )
+parser.add_argument(
+    "-w",
+    dest="wordlist_file",
+    help="The path to the database to be processed",
+    metavar="WORDLIST",
+    type=lambda x: is_valid_file(parser, x),
+)
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("--firefox", action="store_true", help="Process a Firefox database")
 group.add_argument("--chrome", action="store_true", help="Process a Chrome database")
@@ -49,7 +72,7 @@ if args.firefox:
         if i != "":
             record = re.split(r"\t+", i)
             record.append(record[0][-4:])
-            record[0] = re.search(r".+?(?=(\:|\^))", record[0]).group(0)
+            record[0] = re.search(r"^([^:\^]+)", record[0]).group(0)
             record[2] = serial_date_to_string(record[2])
             cleaned = record[3].split(",")
             record[3] = datetime.datetime.fromtimestamp(int(cleaned[0]) / 1000)
@@ -80,6 +103,21 @@ if args.chrome:
                     datetime.datetime.fromtimestamp(current["sts_observed"]),
                 ]
             )
-    printDB(
-        database, ["Base64 URL Hash", "Expiry", "Include Subdomains", "Last Observed"]
-    )
+    if args.wordlist_file:
+        wordlist = args.wordlist_file.read().splitlines()
+        rainbow = []
+        for i in wordlist:
+            rainbow.append(convert(i))
+        for i in database:
+            for j in range(0, len(rainbow)):
+                if i[0] == rainbow[j].decode("utf-8"):
+                    i.append(wordlist[j])
+            if len(i) == 4:
+                i.append("")
+        printDB(
+            database, ["Base64 URL Hash", "Expiry", "Include Subdomains", "Last Observed", "Cracked Hash"]
+        )
+    else:
+        printDB(
+            database, ["Base64 URL Hash", "Expiry", "Include Subdomains", "Last Observed"]
+        )
